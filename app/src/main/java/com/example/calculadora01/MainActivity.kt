fa203ce8ad02b899fd110bdcdbd04d75b8764409
Brandon Medina <brandonmedinalopez1993@gmail.com>
2025-05-16 19:21:14 -0600

    Implementacion de metodos para para simplificar las operaciones y se agregó imagenes y cambio por ImageButton

--- app/src/main/java/com/example/calculadora01/MainActivity.kt
+++ app/src/main/java/com/example/calculadora01/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculadora01
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -17,199 +18,123 @@ class MainActivity : AppCompatActivity() {
      var num2 = 0
      var operacionAsignada = "0"
      var capturaDePantalla = "0"
-    var prueba = "0"
+     lateinit var pantalla2: TextView
+     lateinit var pantalla: TextView
 
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val pantalla = findViewById<TextView>(R.id.textView)
+         pantalla = findViewById<TextView>(R.id.textView)
+         pantalla2 = findViewById<TextView>(R.id.textView2)
 
         val btnCero = findViewById<Button>(R.id.cero)
         btnCero.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "0"
-            }
-            else {
-                pantalla.append("0")
-            }
+           agregarNumero("0")
         }
 
         val btnUno = findViewById<Button>(R.id.btn_uno)
         btnUno.setOnClickListener{
-            if(pantalla.text.toString() == "0"){
-                pantalla.text = "1"
-            }
-            else {
-                pantalla.append("1")  //Apend agrega un nuevo digito
-            }
+           agregarNumero("1")
+              //Apend agrega un nuevo digito
         }
 
         val btnDos = findViewById<Button>(R.id.dos)
         btnDos.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "2"
-            }
-            else{
-                pantalla.append("2")
-            }
+           agregarNumero("2")
         }
 
         val btnTres = findViewById<Button>(R.id.tres)
         btnTres.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "3"
-            }
-            else{
-                pantalla.append("3")
-            }
+            agregarNumero("3")
         }
 
         val btnCuatro = findViewById<Button>(R.id.cuatro)
         btnCuatro.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "4"
-            }
-            else{
-                pantalla.append("4")
-            }
+           agregarNumero("4")
         }
 
         val btnCinco = findViewById<Button>(R.id.cinco)
         btnCinco.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "5"
-            }
-            else{
-                pantalla.append("5")
-            }
+           agregarNumero("5")
         }
 
         val btnSeis = findViewById<Button>(R.id.seis)
         btnSeis.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "6"
-            }
-            else{
-                pantalla.append("6")
-            }
+           agregarNumero("6")
         }
 
         val btnSiete = findViewById<Button>(R.id.siete)
         btnSiete.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "7"
-            }
-            else{
-                pantalla.append("7")
-            }
+           agregarNumero("7")
         }
 
         val btnOcho = findViewById<Button>(R.id.ocho)
         btnOcho.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "8"
-            }
-            else{
-                pantalla.append("8")
-            }
+            agregarNumero("8")
         }
 
         val btnNueve = findViewById<Button>(R.id.nueve)
         btnNueve.setOnClickListener{
-            if (pantalla.text.toString() == "0"){
-                pantalla.text = "9"
-            }
-            else{
-                pantalla.append("9")
-            }
+            agregarNumero("9")
         }
 
 
         val btnClear = findViewById<Button>(R.id.Ac)
         btnClear.setOnClickListener{
-            pantalla.text = "0"
+            pantalla2.text = "0"
+            pantalla.text = ""
         }
 
         /* dropLast metodo que utilizo para lograr borrar de uno en uno*/
 
-        val borradoParcial = findViewById<Button>(R.id.masmenos)
+        val borradoParcial = findViewById<ImageButton>(R.id.masmenos)
         borradoParcial.setOnClickListener{
 
-            if  (pantalla.text.toString().isEmpty()){
-                pantalla.text = "0"
+            if  (pantalla2.text.toString().isEmpty()){
+                pantalla2.text = "0"
             }
             else{
-                pantalla.text = pantalla.text.toString().dropLast(1)
+                pantalla2.text = pantalla2.text.toString().dropLast(1)
             }
         }
 
-        val btnSuma = findViewById<Button>(R.id.suma)
+        val btnSuma = findViewById<ImageButton>(R.id.suma)
         btnSuma.setOnClickListener{
 
-            operacionAsignada = "*"  // Esto funciona porque al darle click la variable es asignada a este valor y sirve para poder hacer despues la validacion.
+             operacionesMatematicas("+")
 
-            capturaDePantalla = pantalla.text.toString()
-             try {
-                 num1 = capturaDePantalla.toInt()
-             } catch (e:NumberFormatException){
-                 println("No se ha ingresado un numero valido")
-             }
-            pantalla.text = "0"
+           /* ultimoValorGuardado = pantalla.text.toString()
+            num1 = ultimoValorGuardado.toInt()*/
 
         }
-         val btnResta = findViewById<Button>(R.id.resta)
+         val btnResta = findViewById<ImageButton>(R.id.resta)
          btnResta.setOnClickListener{
 
-             operacionAsignada = "-"
-
-             capturaDePantalla = pantalla.text.toString()
-
-             try {
-                 num1 = capturaDePantalla.toInt()
-             }catch (e:NumberFormatException){
-                 println("No se ha ingresado un numero valido")
-             }
-
-             pantalla.text = "0"
+             operacionesMatematicas("-")
          }
 
-         val btnDivision = findViewById<Button>(R.id.division)
+         val btnDivision = findViewById<ImageButton>(R.id.division)
          btnDivision.setOnClickListener{
 
-             operacionAsignada = "/"
-
-             capturaDePantalla = pantalla.text.toString()
-
-             try {
-                 num1 = capturaDePantalla.toInt()
-             }catch (e:NumberFormatException){
-                 println("No se ha ingresado un numero valido")
-             }
-
-             pantalla.text = "0"
+             operacionesMatematicas("/")
          }
 
-         var btnMultiplicacion = findViewById<Button>(R.id.multiplicacion)
+         var btnMultiplicacion = findViewById<ImageButton>(R.id.multiplicacion)
          btnMultiplicacion.setOnClickListener{
 
-             operacionAsignada = "*"
-
-             capturaDePantalla = pantalla.text.toString()
-
-
-             try {
-                 num1 = capturaDePantalla.toInt()
-             }catch (e:NumberFormatException){
-                 println("No se ha ingresado un numero valido")
-             }
-
-             pantalla.text = "0"
+             operacionesMatematicas("*")
          }
 
-        val btnResultado = findViewById<Button>(R.id.igual)
+         val btnPunto = findViewById<ImageButton>(R.id.punto)
+         btnPunto.setOnClickListener{
+             pantalla2.append(".")
+         }
+
+        val btnResultado = findViewById<ImageButton>(R.id.igual)
         btnResultado.setOnClickListener(){
-            var segundaCapturaDePantalla = pantalla.text.toString()
+
+            var segundaCapturaDePantalla = pantalla2.text.toString()
 
             try {
                 num2 = segundaCapturaDePantalla.toInt()
@@ -217,18 +142,12 @@ class MainActivity : AppCompatActivity() {
                 println("No se ha ingresado un numero valido")
             }
 
-            if (operacionAsignada == "+"){
-                pantalla.text = suma().toString()
-            }else if (operacionAsignada == "-"){
-                pantalla.text = resta().toString()
-            }else if (operacionAsignada == "/"){
-                pantalla.text = division().toString()
-            }else if (operacionAsignada == "*"){
-                pantalla.text = multiplicaion().toString()
-            }else{
-                println("Operacion no encontrada")
+            when(operacionAsignada){
+                "+" -> pantalla.text = "= ${suma()}"
+                "-" -> pantalla.text = "= ${resta()}"
+                "*" -> pantalla.text = "= ${multiplicaion()}"
+                "/" -> pantalla.text = "= ${division()}"
             }
-
 
         }
 
@@ -257,8 +176,32 @@ class MainActivity : AppCompatActivity() {
         return resultadoMultiplicacion
     }
 
+    fun agregarNumero(numero:String){
+        if (pantalla2.text.toString() == "0"){
+            pantalla2.text = numero
+        }
+        else{
+            pantalla2.append(numero)
+        }
+    }
+
+    fun operacionesMatematicas(operacion: String){
+
+        operacionAsignada = operacion
+        capturaDePantalla = pantalla2.text.toString()
+
+        try {
+            num1 = capturaDePantalla.toInt()
+        }catch (e:NumberFormatException){
+            println("No se ha ingresado un numero valido")
+        }
+
+        pantalla2.text = "0"
+    }
 
  }
+
+
 
 
 
